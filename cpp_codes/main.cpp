0366539542903df25a88ce934298f442d6451bf6
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-09-17 14:51:46 +0200

    added class, header file and main to access it

---
+++ cpp_codes/main.cpp
@@ -0,0 +1,38 @@
+#include "solver.hpp"
+#include <armadillo>
+#include <time.h>
+#include <fstream>
+
+using namespace std;
+using namespace arma;
+
+int main{
+    clock_t start, end;
+    double timeused;
+
+    string filename = "./iterations.txt.";
+    ofsteam ofile;
+
+    int n = 100;
+    double tol = 1e-8;
+    mat A = zeros<mat>(n, n);
+    A.diag().fill(2);
+    A.diag(-1).fill(-1);
+    A.diag(1).fill(-1);
+    
+    Solver my_solver;
+    my_solver.init(n, A, tol);
+    start = clock();
+    my_solver.run();
+    end = clock();
+    timeused = (end-start)/CLOCKS_PER_SEC; 
+    my_solver.write_to_file(filename);
+
+    // Open here again and add timing
+    // This could be done inside the class too. What do 
+    // you think amund?
+    ofile.open(filename);
+    m_ofile << timused << "time_used" << end1;
+    m_ofile.close();
+    return 0;
+}
---
+++ cpp_codes/solver.hpp
@@ -0,0 +1,38 @@
+#infdef SOLVER_HPP
+#define SOLVER_HPP
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <armadillo>
+
+using namespace arma;
+using namespace std;
+
+class Solver{
+
+private:
+    int m_n;
+    mat m_A;
+    mat m_R;
+    double m_tol;
+    int m_max_iter;
+    int m_k;
+    int m_p;
+    double m_tau;
+    bool m_tol_reached;
+    int m_i;
+    ofstream m_ofile;
+
+public:
+    void init(int n, mat A, double tol);
+    void max_off_diag();
+    void calc_tau();
+    void rotate();
+    void write_to_file(string filename);
+    void print_out();
+protected:
+
+
+};
+#endif
