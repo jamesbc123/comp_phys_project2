860174351a6e6ca22225fe3ba86988674899bfa2
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-09-23 09:45:37 +0200

    added loop for many n

--- cpp_codes/main.cpp
+++ cpp_codes/main.cpp
@@ -11,6 +11,8 @@ ofstream ofile;
 
 int main(){
     string filename = "iterations.txt"; // This file saves number of iterations for
+    string filename_timing = "timing.txt";
+
     // different matrix sizes n. It is a .csv file.
     const char * filename_c_str = filename.c_str(); // The "remove(...)" function below requires the input file name to be
     // of the type const char*.
@@ -19,30 +21,35 @@ int main(){
     ofile.open(filename);
     // Choose the data column titles:
     ofile << "n, " << "number_of_transformations" << endl;
-    
     ofile.close();  // The file will be re-opened in the function write_to_file.
     
     clock_t start, end;
     double timeused;
 
-    int n = 100;
+    int pow = 5;
     double tol = 1e-8;
-    mat A = zeros<mat>(n, n);
-    A.diag().fill(2);
-    A.diag(-1).fill(-1);
-    A.diag(1).fill(-1);
-    
-    Solver my_solver;
-    cout << "n (before .init()): " << n << endl;
-    my_solver.init(n, A, tol);
 
-    // Run the algorithm and time it:
-    start = clock();
-    my_solver.run();
-    end = clock();
-    timeused = (end-start)/CLOCKS_PER_SEC;
-    cout << "n = " << n << ", time used by my_solver.run() (seconds): " << timeused << endl;
+    for(int i=1; i < pow; i++){
+        int n = 10^pow;
+        mat A = zeros<mat>(n, n);
+        A.diag().fill(2);
+        A.diag(-1).fill(-1);
+        A.diag(1).fill(-1);
+        
+        Solver my_solver;
+        cout << "n (before .init()): " << n << endl;
+        my_solver.init(n, A, tol);
 
-    my_solver.write_to_file(filename);  // Write 
+        // Run the algorithm and time it:
+        start = clock();
+        my_solver.run();
+        end = clock();
+        timeused = (end-start)/CLOCKS_PER_SEC;
+
+        ofile.open(filename_timing);
+        ofile << "n = " << n << ", time used by my_solver.run() (seconds): " << timeused << endl;
+        ofile.close();
+
+        my_solver.write_to_file(filename);  // Write 
     return 0;
 }
