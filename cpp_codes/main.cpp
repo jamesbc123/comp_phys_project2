a49b8d2c3974b32f625741712992b51d96c4eeda
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-09-23 14:34:42 +0200

    loop over many powers

--- cpp_codes/main.cpp
+++ cpp_codes/main.cpp
@@ -18,38 +18,52 @@ int main(){
     // of the type const char*.
     cout << "remove(filename_c_str) = " << remove(filename_c_str) << endl;   // Remove the file if it already exists.
     
+    // Do the same for the timing file.
+    const char * filename_t_c_str = filename_timing.c_str();
+    cout << "remove(filename_t_c_str) = " << remove(filename_t_c_str) << endl; 
+
     ofile.open(filename);
     // Choose the data column titles:
     ofile << "n, " << "number_of_transformations" << endl;
     ofile.close();  // The file will be re-opened in the function write_to_file.
+
+    ofile.open(filename_timing);
+    ofile << "n," << "time used by solver class (seconds)" << endl;
+    ofile.close();  
     
     clock_t start, end;
     double timeused;
 
-    int pow = 5;
+    int power = 3;
     double tol = 1e-8;
+    int repeatition = 5;
 
-    for(int i=1; i < pow; i++){
-        int n = 10^pow;
-        mat A = zeros<mat>(n, n);
-        A.diag().fill(2);
-        A.diag(-1).fill(-1);
-        A.diag(1).fill(-1);
-        
-        Solver my_solver;
-        cout << "n (before .init()): " << n << endl;
-        my_solver.init(n, A, tol);
+    for(int j=0; j<repeatition; j++){
+        for(int i=1; i < power; i++){
+            int n = pow(10, i);
+            mat A = zeros<mat>(n, n);
+            A.diag().fill(2);
+            A.diag(-1).fill(-1);
+            A.diag(1).fill(-1);
+            
+            Solver my_solver;
+            cout << "n (before .init()): " << n << endl;
+            my_solver.init(n, A, tol);
 
-        // Run the algorithm and time it:
-        start = clock();
-        my_solver.run();
-        end = clock();
-        timeused = (end-start)/CLOCKS_PER_SEC;
+            // Run the algorithm and time it:
+            start = clock();
+            my_solver.run();
+            end = clock();
+            timeused = ((double)end-(double)start)/CLOCKS_PER_SEC;
+            cout <<scientific<< "time used" << timeused << endl;
+            ofile.open(filename_timing, ios::app);
+            ofile << "\n" << n << "," << timeused << endl;
+            ofile.close();
 
-        ofile.open(filename_timing);
-        ofile << "n = " << n << ", time used by my_solver.run() (seconds): " << timeused << endl;
-        ofile.close();
-
-        my_solver.write_to_file(filename);  // Write 
+            string filename_R = ""
+            my_solver.write_to_file(filename, filename_R);  // Write 
+        }
+    }
+    
     return 0;
 }
