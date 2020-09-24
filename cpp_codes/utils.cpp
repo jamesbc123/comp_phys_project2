9090a78000de22f8feea5bde4b6e2a3ed33fc620
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2020-09-24 15:48:41 +0200

    Update utils.cpp

--- cpp_codes/utils.cpp
+++ cpp_codes/utils.cpp
@@ -17,9 +17,9 @@ using namespace arma;
 
 void run_problem_c(){
     ofstream ofile;
-    
-    string filename = "iterations.txt"; // This file saves number of iterations for
-    string filename_timing = "timing.txt";
+
+    string filename = "../results/buckling_beam/iterations.txt"; // This file saves number of iterations for
+    string filename_timing = "../results/buckling_beam/timing.txt";
 
     // different matrix sizes n. It is a .csv file.
     const char * filename_c_str = filename.c_str(); // The "remove(...)" function below requires the input file name to be
@@ -36,19 +36,19 @@ void run_problem_c(){
     ofile.close();  // The file will be re-opened in the function write_to_file.
 
     ofile.open(filename_timing);
-    ofile << "n, " << "time used by solver class (seconds)" << endl;
+    ofile << "n,time_used,time_used_arma" << endl;
     ofile.close();  
     
     clock_t start, end;
     double timeused;
+    double timeused_arma;
 
-    int maxPower = 2;
     double tol = 1e-8;
     int repetition = 1;
+    int max_n = 150;
 
     for(int j=0; j<repetition; j++){
-        for(int i=1; i <= maxPower; i++){
-            int n = pow(10, i);
+        for(int n=50; n< max_n; n+=50){
             mat A = zeros<mat>(n, n);
             A.diag().fill(2);
             A.diag(-1).fill(-1);
@@ -57,19 +57,34 @@ void run_problem_c(){
             Solver my_solver;
             my_solver.init(n, A, tol);
 
-            // Run the algorithm and time it:
+            // Run the algorithm and time it.
             start = clock();
             my_solver.run();
             end = clock();
             timeused = ((double)end-(double)start)/CLOCKS_PER_SEC;
-            cout <<scientific<< "time used" << timeused << endl;
+
+            // Run armadillo solver and time it.
+            mat D = zeros<mat>(n, n);
+            D = diagmat(A);
+            vec eigval;
+            mat eigvec;
+            start = clock();
+            eig_sym(eigval, eigvec, A);
+            end = clock();
+            timeused_arma = ((double)end-(double)start)/CLOCKS_PER_SEC;
+
+            // Write timings to file.
+            cout <<scientific<< "time used by solver class" << timeused << endl;
             ofile.open(filename_timing, ios::app);
-            ofile << "\n" << n << "," << timeused << endl;
+            ofile << n << "," << timeused << "," << timeused_arma << endl;
             ofile.close();
 
+            // Sort the eigenvectors and eigenvalues from solver class.
             my_solver.sort_eigvec_and_eigval();
-            string filename_R = "eig_vec_"+ to_string(n) + ".txt";
+            string filename_R = "../results/buckling_beam/eig_vec_"+ to_string(n) + ".txt";
             my_solver.write_to_file(filename, filename_R);  // Write 
+        }
+    }
         }
     }
 }
